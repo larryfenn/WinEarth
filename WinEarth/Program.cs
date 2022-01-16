63084382887623df6de33bda049c6c0600ce64a8
Larry Fenn <larry.fenn@gmail.com>
2022-01-16 00:10:01 -0500

    Add project files.

---
+++ WinEarth/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Net;
+
+namespace WinEarth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wallpaper[] screens = { new Wallpaper(2), new Wallpaper(0), new Wallpaper(1) }; // order comes from the monitor order in Displays
+
+            string[] urls =
+            {
+                "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/eus/GEOCOLOR/20220160441_GOES16-ABI-eus-GEOCOLOR-2000x2000.jpg",
+                "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/CONUS/GEOCOLOR/20220160441_GOES16-ABI-CONUS-GEOCOLOR-2500x1500.jpg",
+                "https://cdn.star.nesdis.noaa.gov/GOES16/ABI/SECTOR/ne/GEOCOLOR/20220160441_GOES16-ABI-ne-GEOCOLOR-2400x2400.jpg"
+            };
+            string[] filenames = { "left.png", "center.png", "right.png"};
+            string storagePath = @"C:\Users\larry\Downloads\Desktop\EarthView";
+
+            using (var client = new WebClient())
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    string storageFilename = filenames[i];
+                    string picFilename = Path.Combine(storagePath, storageFilename);
+                    client.DownloadFile(urls[i], picFilename); // TODO: handle HTTP errors
+                    screens[i].Set(picFilename);
+                }
+            }
+        }
+    }
+}
---
+++ WinEarth/Wallpaper.cs
@@ -0,0 +1,27 @@
+﻿namespace WinEarth
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    public class Wallpaper
+    {
+        private readonly string monitor;
+        private readonly IDesktopWallpaper wallpaper;
+        public Wallpaper(uint monitorIndex) {
+            wallpaper = new DesktopWallpaper() as IDesktopWallpaper;
+            monitor = wallpaper.GetMonitorDevicePathAt(monitorIndex);
+        }
+
+        public void Set(string file, string storagePath)
+        {
+            Image img = Image.FromFile(file);
+            img.Save(storagePath, ImageFormat.Png);
+            wallpaper.SetWallpaper(monitor, storagePath);
+        }
+
+        public void Set(string file)
+        {
+            wallpaper.SetWallpaper(monitor, file);
+
+        }
+    }
+}
