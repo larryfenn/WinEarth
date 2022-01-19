860bf276de6a101d6887c572261b6fcb9b6f9bf0
Larry Fenn <larry.fenn@gmail.com>
2022-01-19 18:43:45 -0500

    Actually use Tasks correctly & cut down on web clients

--- WinEarth/Program.cs
+++ WinEarth/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -12,14 +13,19 @@ namespace WinEarth
     class Program
     {
         private static string storagePath = @"C:\Users\larry\Downloads\Desktop\WinEarth";
+        private static HttpClient pageScrapeClient = new HttpClient();
+
         static void Main(string[] args)
         {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            pageScrapeClient.DefaultRequestHeaders.Accept.Clear();
             Wallpaper[] screens = { new Wallpaper(2), new Wallpaper(0), new Wallpaper(1) }; // order comes from the monitor order in Displays
-            string[] page_urls =
+            WebClient[] clients = { new WebClient(), new WebClient(), new WebClient() };
+            Uri[] page_urls =
             {
-                    "https://www.star.nesdis.noaa.gov/GOES/conus.php?sat=G16",
-                    "https://www.star.nesdis.noaa.gov/GOES/sector.php?sat=G16&sector=eus",
-                    "https://www.star.nesdis.noaa.gov/GOES/sector.php?sat=G16&sector=ne"
+                    new Uri("https://www.star.nesdis.noaa.gov/GOES/conus.php?sat=G16"),
+                    new Uri("https://www.star.nesdis.noaa.gov/GOES/sector.php?sat=G16&sector=eus"),
+                    new Uri("https://www.star.nesdis.noaa.gov/GOES/sector.php?sat=G16&sector=ne")
             };
             string[] filenames = { "left.png", "center.png", "right.png" };
             List<Task> tasks = new List<Task>();
@@ -27,28 +33,16 @@ namespace WinEarth
             {
                 for (int i = 0; i < 3; i++)
                 {
-                    tasks.Add(DownloadImageFileAsync(page_urls[i], Path.Combine(storagePath, filenames[i]), screens[i]));
+                    tasks.Add(DownloadImageFileAsync(page_urls[i], Path.Combine(storagePath, filenames[i]), screens[i], clients[i]));
                 }
-                Task.WhenAll(tasks);
+                Task.WhenAll(tasks).Wait();
                 Thread.Sleep(300000);
             }
         }
-        private static async Task DownloadImageFileAsync(string fullUrl, string filePath, Wallpaper screen)
+        private static async Task DownloadImageFileAsync(Uri fullUrl, string filePath, Wallpaper screen, WebClient client)
         {
-            string imageUrl;
-            using (HttpClient client = new HttpClient())
-            {
-                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-                client.DefaultRequestHeaders.Accept.Clear();
-                var response = client.GetStringAsync(fullUrl);
-                imageUrl = GetImageUrl(await response);
-            }
-
-            using (var client = new WebClient())
-            {
-                await client.DownloadFileTaskAsync(imageUrl, filePath);
-            }
-
+            string imageUrl = GetImageUrl(await client.DownloadStringTaskAsync(fullUrl));
+            await client.DownloadFileTaskAsync(imageUrl, filePath);
             screen.Set(filePath);
         }
         private static string GetImageUrl(string html)
@@ -56,8 +50,7 @@ namespace WinEarth
             HtmlDocument htmlDoc = new HtmlDocument();
             htmlDoc.LoadHtml(html);
             var table = htmlDoc.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "").Contains("Links")).ToList();
-            string url = table[0].Descendants("a").ToArray<HtmlNode>()[4].Attributes["href"].Value;
-            return url;
+            return table[0].Descendants("a").ToArray<HtmlNode>()[4].Attributes["href"].Value;
         }
     }
 }
